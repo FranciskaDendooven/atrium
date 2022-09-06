59fcab0d71282a9748d1611d9387c011bed42b44
Gladys Haelters <haelters_gladys@hotmail.com>
2022-09-06 11:51:58 +0200

    Just commiting

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,12 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('brainfarts', [BrainFartsController::class, 'show'])->name("brainFart");
+
+
+
+
 require __DIR__.'/auth.php';
+
+
