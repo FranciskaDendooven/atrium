374394d85c6fb43f66d009abfe69edf563488886
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-09-30 17:50:16 +0200

    Style the editprofile page

--- resources/js/Pages/EditUserSettings.jsx
+++ resources/js/Pages/EditUserSettings.jsx
@@ -21,7 +21,7 @@ export default function UserSettings(props) {
             <Head title="User Settings" />
             <div className="grid justify-center py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div  id="changeUserSettingspage" className="w-[700px] bg-white overflow-hidden shadow-sm rounded-xl border-2 border-darkblue sm:rounded-lg">
+                <div  id="changeUserSettingspage" className="w-[700px] bg-white overflow-hidden shadow-sm rounded-xl border-2 border-darkblue sm:rounded-lg p-8">
                    <h1 className="my-4 font-bold">Change your settings</h1>
     
                          <section className="flex items-center justify-between mb-6">
@@ -33,14 +33,14 @@ export default function UserSettings(props) {
                                 </Link>
                             </section>
             
-                    <section className="flex flex-col justify-center w-96 mx-20 my-10">
+                    <section className="flex flex-col">
                         <form name="updateUserSettings" onSubmit={handleSubmit}>
                             <div className="flex flex-col">
                                 <div className="mb-4">
                                     <label className="">User Name</label>
                                     <input
                                         type="text"
-                                        className="mt-1 block w-full"
+                                        className="w-full px-4 py-2 rounded-md"
                                         label="UserName"
                                         name="username"
                                         value={data.username}
@@ -54,11 +54,12 @@ export default function UserSettings(props) {
                                     </span>
                                 </div>
 
-                                <div className="mb-0 mt-4">
+                                <div className="flex flex-col mt-4">
+                                <div className="mb-0">
                                     <label className="">Email</label>
                                     <input
                                         type="email"
-                                        className="mt-1 block w-full"
+                                        className="w-full px-4 py-2 rounded-md"
                                         label="email"
                                         name="email"
                                         value={data.email}
@@ -72,11 +73,12 @@ export default function UserSettings(props) {
                                     </span>
                                 </div>
                             </div>
+                            </div>
 
                             <div className="mt-4">
                                 <button
                                     type="submit"
-                                    className="px-6 py-2 font-bold text-white bg-lightBlue rounded"
+                                    className="py-2 px-3 ml-2 rounded-full text-white text-l bg-lightBlue focus:outline-none"
                                 >
                                     Update
                                 </button>
