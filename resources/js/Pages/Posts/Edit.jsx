d08dc7bc923f8ccd7705e10b56875c9d9aebe044
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-09-30 10:44:37 +0200

    Finish styling the create and edit page

--- resources/js/Pages/Posts/Edit.jsx
+++ resources/js/Pages/Posts/Edit.jsx
@@ -23,14 +23,14 @@ export default function Dashboard(props) {
        <Navbar auth={props.auth}errors={props.errors}></Navbar>
         <Head title="Posts" />
 
-            <div className="py-12 min-w-full">
+            <div className="grid justify-center py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm rounded-xl border-2 border-darkblue sm:rounded-lg">
+                    <div id="editForm" className="w-[700px] bg-white overflow-hidden shadow-sm rounded-xl border-2 border-darkblue sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
 
                             <div className="flex items-center justify-between mb-6">
                                 <Link
-                                    className="px-6 py-2 text-white bg-redOrange rounded-full focus:outline-none"
+                                    className="py-2 px-3 ml-2 rounded-full text-white text-l bg-redOrange focus:outline-none"
                                     href={route("showPosts")}
                                 >
                                     Back
@@ -43,7 +43,7 @@ export default function Dashboard(props) {
                                         <label className="">Title</label>
                                         <input
                                             type="text"
-                                            className="w-full px-4 py-2"
+                                            className="w-full px-4 py-2 rounded-md"
                                             label="Title"
                                             name="title"
                                             value={data.title}
@@ -56,13 +56,15 @@ export default function Dashboard(props) {
                                             </span>
                                     </div>
                                     
-                                    <div className="mb-0 mt-4">
+                                    <div className="flex flex-col mt-4">
+                                    <div className="mb-0">
                                         <label className="">Content</label>
                                         <textarea
                                             type="text"
-                                            className="w-full rounded"
+                                            className="w-full px-4 py-2 rounded-md"
                                             label="content"
                                             name="content"
+                                            row="15"
                                             errors={errors.content}
                                             value={data.content}
                                             onChange={(e) =>
@@ -73,12 +75,14 @@ export default function Dashboard(props) {
                                             {errors.content}
                                         </span>
                                     </div>
+                                    </div>
 
-                                    <div className="mb-0 mt-4">
+                                    <div className="flex flex-col mt-4">
+                                    <div className="mb-0">
                                         <label className="">Tag</label>
-                                        <textarea
+                                        <input
                                             type="text"
-                                            className="w-full rounded"
+                                            className="w-full px-4 py-2 rounded-md"
                                             label="tag"
                                             name="tag"
                                             errors={errors.tag}
@@ -92,11 +96,12 @@ export default function Dashboard(props) {
                                         </span>
                                     </div>
                                 </div>
+                                </div>
 
                                 <div className="mt-4">
                                     <button
                                         type="submit"
-                                        className="px-6 py-2 font-bold text-white bg-lightBlue rounded"
+                                        className="py-2 px-3 ml-2 rounded-full text-white text-l bg-lightBlue focus:outline-none"
                                     >
                                         Update
                                     </button>
