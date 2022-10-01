3c1d0b5eaaa7e8b8e80aa0582baedfdf9f4c97fa
Gladys Haelters <haelters_gladys@hotmail.com>
2022-10-01 21:18:18 +0200

    Recreate form and styling

--- resources/js/Components/Form.jsx
+++ resources/js/Components/Form.jsx
@@ -3,163 +3,118 @@ import React from "react";
 export default function Form() {
     return (
         <>
-            <div className="col-span-6 sm:col-span-3">
-                <h3>Button Bot</h3>
-                <label
-                    htmlFor="Morning Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    Morning Button Message
-                </label>
-                <input
-                    type="text"
-                    name="Morning Button Message"
-                    id="morning"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+            <section className="flex flex-col">
+                <section className="items-center justify-center font-bold">
+                    <h2 className="text-3xl">Button Bot</h2>
+                </section>
+                <form name="buttonBot">
+                    <div className="flex flex-col">
+                        <div className="m-4">
+                            <label className="">Morning Button</label>
+                            <textarea
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="Morning"
+                                name="Morning"
+                            />
+                        </div>
 
-            <div className="col-span-6 sm:col-span-3">
-                <label
-                    htmlFor="Lunch Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    Lunch Button Message
-                </label>
-                <input
-                    type="text"
-                    name="Lunch Button Message"
-                    id="lunch"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+                        <div className="m-4">
+                            <label className="">Lunch Button</label>
+                            <textarea
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="Lunch"
+                                name="Lunch"
+                            />
+                        </div>
 
-            <div className="col-span-6 sm:col-span-3">
-                <label
-                    htmlFor="Afternoon Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    Afternoon Button Message
-                </label>
-                <input
-                    type="text"
-                    name="Afternoon Button Message"
-                    id="Afternoon"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+                        <div className="m-4">
+                            <label className="">Afternoon Button</label>
+                            <textarea
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="Afternoon"
+                                name="Afternoon"
+                            />
+                        </div>
 
-            <div className="col-span-6 sm:col-span-3">
-                <label
-                    htmlFor="Evening Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    Evening Button Message
-                </label>
-                <input
-                    type="text"
-                    name="Evening Button Message"
-                    id="Evening"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+                        <div className="m-4">
+                            <label className="">Evening Button</label>
+                            <textarea
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="evening"
+                                name="evening"
+                            />
+                        </div>
+                    </div>
 
-            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    Save
-                </button>
-            </div>
+                    <div className="mt-4">
+                        <button
+                            type="submit"
+                            className="py-2 px-3 ml-2 rounded-full text-white text-l bg-lightBlue focus:outline-none"
+                        >
+                            Send!
+                        </button>
+                    </div>
+                </form>
+            </section>
 
             <br />
             <br />
             <br />
             <br />
-             <h3>Compliment Bot</h3>
-            <div className="col-span-6 sm:col-span-3">
-               
-                <label
-                    htmlFor="coachC"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    Which coach 
-                </label>
-                <input
-                    type="text"
-                    name="coachC"
-                    id="coach"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
 
-            <div className="col-span-6 sm:col-span-3">
-                <label
-                    htmlFor="Lunch Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    From who is the compliment
-                </label>
-                <input
-                    type="text"
-                    name="Lunch Button Message"
-                    id="lunch"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+            <section className="flex flex-col">
+                <section className="items-center justify-center font-bold">
+                    <h2 className="text-3xl">Compliment Bot</h2>
+                </section>
+                <form name="buttonBot">
+                    <div className="flex flex-col">
+                        <div className="m-4">
+                            <label className="">Who'd you like to compliment?</label>
+                            <input
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="Who"
+                                name="Who"
+                         />
+                        </div>
 
-            <div className="col-span-6 sm:col-span-3">
-                <label
-                    htmlFor="Afternoon Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    compliment
-                </label>
-                <input
-                    type="text"
-                    name="Afternoon Button Message"
-                    id="Afternoon"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+                        <div className="m-4">
+                            <label className="">What would you like to say?</label>
+                            <textarea
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="Compliment"
+                                name="Compliment"
+                            />
+                        </div>
 
-            <div className="col-span-6 sm:col-span-3">
-                <label
-                    htmlFor="Evening Button Message"
-                    className="block text-sm font-medium text-gray-900"
-                >
-                    Evening Button Message
-                </label>
-                <input
-                    type="text"
-                    name="Evening Button Message"
-                    id="Evening"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                />
-            </div>
+                        <div className="m-4">
+                            <label className="">From who?</label>
+                            <input
+                                type="text"
+                                className="w-full px-4 py-2 rounded-md"
+                                label="FromWho"
+                                name="FromWho"
+                            />
+                        </div>
 
-            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    Save
-                </button>
-            </div>
+                    </div>
 
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-
-         
-ù        </>
+                    <div className="mt-4">
+                        <button
+                            type="submit"
+                            className="py-2 px-3 ml-2 rounded-full text-white text-l bg-lightBlue focus:outline-none"
+                        >
+                            Send!
+                        </button>
+                    </div>
+                </form>
+            </section>
+           
+        </>
     );
 }
