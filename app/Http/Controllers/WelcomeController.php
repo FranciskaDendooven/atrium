68289470a26f876e23cfceebe6ac290603b39b99
madalenaRio <madalena.rio@gmail,com>
2022-10-02 12:11:14 +0200

    fix to welcome page controler - latest post from each category is displayed

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,11 +12,42 @@ class WelcomeController extends Controller
 {
     public function load()
     {
-        $posts = Post::latest('updated_at')->first()
+        $techPost = Post::latest('updated_at')
+            ->where('page', 'TechNews')
             ->with(['comments'])
             ->with('comments.user')
             ->with('user')
-            ->get();
+            ->first();
+        $CodeSharePost = Post::latest('updated_at')
+            ->where('page', 'Code Share')
+            ->with(['comments'])
+            ->with('comments.user')
+            ->with('user')
+            ->first();
+        $pifPost = Post::latest('updated_at')
+            ->where('page','PIF')
+            ->with(['comments'])
+            ->with('comments.user')
+            ->with('user')
+            ->first();
+        $brainFartsPost = Post::latest('updated_at')
+            ->where('page','BrainFarts')
+            ->with(['comments'])
+            ->with('comments.user')
+            ->with('user')
+            ->first();
+        $codeQAPost = Post::latest('updated_at')
+            ->where('page','CodeQA')
+            ->with(['comments'])
+            ->with('comments.user')
+            ->with('user')
+            ->first();
+
+        $posts = [];
+        array_push($posts, $techPost, $CodeSharePost, $pifPost, $brainFartsPost, $codeQAPost);
+
+       // dd($posts);
+
         return Inertia::render('Welcome', [
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
