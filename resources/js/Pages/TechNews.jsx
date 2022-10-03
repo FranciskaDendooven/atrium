38c7b1ac16f1e144db93b9eea6989d91b801d096
Gladys Haelters <haelters_gladys@hotmail.com>
2022-10-03 09:13:19 +0200

    fix styling error

--- resources/js/Pages/TechNews.jsx
+++ resources/js/Pages/TechNews.jsx
@@ -99,7 +99,7 @@ export default function Posts(props) {
                 </section>
             </div>
 
-            <div className="py-12 mx-[10%]">
+            <div className="py-12 mx-[20%]">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
